da9a1f2137ca0eb788901f57e58ae87b917df023
jamm0316 <jamm0316@naver.com>
2025-05-08 09:00:13 +0900

    feat: 사용자 정보 받아오기
    1. Controller에서 BaseResponse 반환
    2. Service에서 KakaoOAuthClient 호출
    3. KakaoOAuthClient에서 UserInfo요청 후 반환
    
    Resolves #8

--- src/main/java/com/evan/wearesikgu/domain/member/oauth2/KakaoUserInfoResponseDTO.java
+++ src/main/java/com/evan/wearesikgu/domain/member/oauth2/KakaoUserInfoResponseDTO.java
@@ -3,7 +3,6 @@ package com.evan.wearesikgu.domain.member.oauth2;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Profile;
 
 @Getter
 @NoArgsConstructor
