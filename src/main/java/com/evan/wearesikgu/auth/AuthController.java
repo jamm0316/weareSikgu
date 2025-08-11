614554211c376dc41773a6ee69b07db7c7703b73
jamm0316 <jamm0316@naver.com>
2025-08-11 17:17:25 +0900

    refactor: AuthController, OAuthController Param 수정
    1. uaHash, ipPrefix 추가
    
    Resolves #30

--- src/main/java/com/evan/wearesikgu/auth/AuthController.java
+++ src/main/java/com/evan/wearesikgu/auth/AuthController.java
@@ -4,8 +4,10 @@ import com.evan.wearesikgu.auth.oauth.OAuthUserInfo;
 import com.evan.wearesikgu.common.baseResponse.BaseResponse;
 import com.evan.wearesikgu.common.baseResponse.BaseResponseStatus;
 import com.evan.wearesikgu.common.util.CookieUtil;
+import com.evan.wearesikgu.common.util.FingerprintUtil;
 import com.evan.wearesikgu.config.token.TokenResponse;
 import com.evan.wearesikgu.config.token.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +21,25 @@ public class AuthController {
     private final TokenService tokenService;
 
     @PostMapping("/login")
-    public BaseResponse<Object> login(@RequestBody OAuthUserInfo userInfo) {
-        TokenResponse jwt = authService.login(userInfo);
+    public BaseResponse<Object> login(
+            @RequestBody OAuthUserInfo userInfo,
+            HttpServletRequest request) {
+
+        String uaHash = FingerprintUtil.uaHash(request.getHeader("User-Agent"));
+        String ipPrefix = FingerprintUtil.ipPrefix(FingerprintUtil.extractClientIp(request));
+        TokenResponse jwt = authService.login(userInfo, uaHash, ipPrefix);
         return new BaseResponse<>(jwt);
     }
 
     @PostMapping("/reissue")
     public BaseResponse<Object> reissue(
             @CookieValue String accessToken,
-            @CookieValue String refreshToken) {
-        String newAccessToken = tokenService.reissueAccessToken(accessToken, refreshToken);
+            @CookieValue String refreshToken,
+            HttpServletRequest request) {
+        String uaHash = FingerprintUtil.uaHash(request.getHeader("User-Agent"));
+        String ipPrefix = FingerprintUtil.ipPrefix(FingerprintUtil.extractClientIp(request));
+
+        String newAccessToken = tokenService.reissueAccessToken(accessToken, refreshToken, uaHash, ipPrefix);
         return new BaseResponse<>(newAccessToken);
     }
 
