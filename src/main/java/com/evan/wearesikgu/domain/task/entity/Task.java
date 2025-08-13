51ede4d261d365918bca5982f541fe7e65d61246
jamm0316 <jamm0316@naver.com>
2025-08-13 15:39:05 +0900

    refactor: Task 엔티티 수정: 필드 및 validation 추가
    Task 엔티티에 color, title, startDate, dueDate 등의 필드를 추가하고, 필요한 validation 및 기본값 설정을 적용했습니다. DayLabel을 Day로 변경하고, isDone 대신 status 필드를 도입하여 상태 관리 방식을 개선했습니다.
    
    Resolves #32

--- src/main/java/com/evan/wearesikgu/domain/task/entity/Task.java
+++ src/main/java/com/evan/wearesikgu/domain/task/entity/Task.java
@@ -1,12 +1,16 @@
 package com.evan.wearesikgu.domain.task.entity;
 
-import com.evan.wearesikgu.common.enums.Day;
+import com.evan.wearesikgu.common.enums.DayLabel;
 import com.evan.wearesikgu.common.enums.Priority;
+import com.evan.wearesikgu.common.enums.Status;
 import com.evan.wearesikgu.common.superentity.SuperEntity;
+import com.evan.wearesikgu.domain.colors.Color;
 import com.evan.wearesikgu.domain.project.entity.Project;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,17 +24,43 @@ public class Task extends SuperEntity {
     @JoinColumn(name = "project_id", nullable = false)
     private Project project;
 
-    private String description;
+    @NotNull(message = "color는 필수 입니다.")
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "color_id", nullable = false)
+    private Color color;
 
-    private LocalDateTime due_date;
-
-    private String category;
-
+    @NotNull(message = "priority는 필수 입니다.")
     @Enumerated(EnumType.STRING)
-    private Day dayLabel;
-
-    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(10) default 'HIGH'")
     private Priority priority;
 
-    private boolean isDone;
+    @NotNull(message = "제목은 필수입니다.")
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private DayLabel day;
+
+    private LocalDate startDate;
+
+    private LocalDateTime startTime;
+
+    @NotNull(message = "시작 시간 사용 여부는 필수입니다.")
+    @Column(columnDefinition = "boolean default false")
+    private Boolean startTimeEnabled;
+
+    private LocalDate dueDate;
+
+    private LocalDateTime dueTime;
+
+    @NotNull(message = "마감 시간 사용 여부는 필수입니다.")
+    @Column(columnDefinition = "boolean default false")
+    private Boolean dueTimeEnabled;
+
+    @NotNull(message = "상태는 필수 입니다.")
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(10) default 'SCHEDULE'")
+    @Length(max = 10)
+    private Status status;
 }
