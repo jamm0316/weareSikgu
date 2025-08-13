34141b68027fe416cc5af620c07a8a2806219fc9
jamm0316 <jamm0316@naver.com>
2025-08-13 15:16:37 +0900

    style: 클래스 이름 수정: Colors → Color
    클래스 이름을 복수형에서 단수형인 Color로 변경했습니다. 해당 변경은 클래스의 의미를 더욱 명확히 전달하기 위함입니다.
    
    Resolves #32

--- src/main/java/com/evan/wearesikgu/domain/colors/Color.java
+++ src/main/java/com/evan/wearesikgu/domain/colors/Color.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-public class Colors {
+public class Color {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
