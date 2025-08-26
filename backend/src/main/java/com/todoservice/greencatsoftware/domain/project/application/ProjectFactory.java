06ba254088d8b9f0caa9ab065e4650739306557e
jamm0316 <jamm0316@naver.com>
2025-08-26 14:20:25 +0900

    feat: 멤버 서비스 통합 및 ProjectFactory 수정
    1. MemberService 의존성 추가 및 DI 구성한다.
    2. JWT를 활용해 멤버 조회 로직 추가한다.
    3. member 필드를 포함하도록 Project 생성 메서드 수정한다.
    
    Resolves #128

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectFactory.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectFactory.java
@@ -2,6 +2,8 @@ package com.todoservice.greencatsoftware.domain.project.application;
 
 import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.color.application.ColorService;
+import com.todoservice.greencatsoftware.domain.member.application.MemberService;
+import com.todoservice.greencatsoftware.domain.member.domain.entity.Member;
 import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
 import com.todoservice.greencatsoftware.domain.project.domain.vo.Period;
 import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectCreateRequest;
@@ -11,18 +13,21 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ProjectFactory {
+    private final MemberService memberService;
     private final ColorService colorService;
 
     public Project createProject(ProjectCreateRequest request) {
+        String jwt = "1 test@email 1234 null testname";
+        Member member = memberService.getMemberByIdOrThrow(Long.parseLong(jwt.substring(0, 1)));
         Color color = colorService.getColorByIdOrThrow(request.colorId());
         Period period = Period.of(request.period().startDate(), request.period().endDate(),
                                   request.period().actualEndDate());
 
         return (period.isNull())
-                ? Project.create(color, request.name(), request.status(),
+                ? Project.create(color, member, request.name(), request.status(),
                 request.description(), request.isPublic(), request.visibility())
 
-                : Project.createWithPeriod(color, request.name(), request.status(),
+                : Project.createWithPeriod(color, member, request.name(), request.status(),
                 period, request.description(), request.isPublic(), request.visibility());
     }
 }
