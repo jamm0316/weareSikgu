b9fcf6d1b2b86023e1ddb6c61f4a22bd7ea38bf0
jamm0316 <jamm0316@naver.com>
2025-09-08 17:05:10 +0900

    feat: commit 최신화
    Resolves #133

--- backend/src/main/java/com/todoservice/greencatsoftware/common/util/CookieUtil.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/common/util/CookieUtil.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
-    private static final int ACCESS_TOKEN_MAX_AGE_IN_SECONDS = 60 * 15;  //15분
+    private static final int ACCESS_TOKEN_MAX_AGE_IN_SECONDS = 60 * 15;  //15분.
     private static final int REFRESH_TOKEN_MAX_AGE_IN_SECONDS = 60 * 60 * 24 * 7;  //7일
 
 
